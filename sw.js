339ef870a67c7946bb4bc0634df871e9b4dcbf2f
isdmg <morgenunlimited@gmail.com>
2026-08-18 12:04:41 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+/* Kill-Switch: entfernt eine evtl. früher registrierte Service-Worker-Version
+   samt Caches und deregistriert sich selbst. */
+self.addEventListener("install", (e) => self.skipWaiting());
+self.addEventListener("activate", (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((k) => caches.delete(k)));
+    await self.registration.unregister();
+    const clients = await self.clients.matchAll({ type: "window" });
+    clients.forEach((c) => c.navigate(c.url));
+  })());
+});
